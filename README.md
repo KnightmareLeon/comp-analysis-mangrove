92478da4616fee6dc3ccc45fce1c4fa0c1077bbb
KnightmareLeon <leonardjohncorpuz20@gmail.com>
2026-05-29 18:28:29 +0800

    feat: add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Final Paper Code for CSC133 - Modelling and Simulation
+
+This repository holds the code of the computational framework for comparing mangrove species for the research paper titled "**A One-Dimensional Drag-Based Computational Framework for Wave Attenuation Across Mangrove
+Species**."
+
+This is for complying the final requirements for the subject **CSC133 - Modelling and Simulation**.
+
+Author: Leonard John T. Corpuz
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from simulation import BatchSimulator
 from comparison import Comparator
+
 if __name__ == "__main__":
 
     simulator = BatchSimulator()
@@ -20,9 +21,8 @@ if __name__ == "__main__":
     comparator.plot_energy_loss()
 
     controlled_output_files, controlled_output_dir = simulator.run_controlled_simulations(
-        variable="veg_end",
-        values=list(range(200, 105, -5)),
-        keep_width=True
+        variable="d",
+        values=[0.25,0.5,0.75,1.0,1.25,1.5,1.75,2.0,2.25,2.5]
     )
 
     controlled_comparator = Comparator(
@@ -31,11 +31,11 @@ if __name__ == "__main__":
     )
 
     controlled_comparator.plot_controlled_line(
-        x_variable="vegetation_cross_shore_distance",
+        x_variable="depth",
         y_variable="wave_reduction"
     )
 
     controlled_comparator.plot_controlled_line(
-        x_variable="vegetation_cross_shore_distance",
+        x_variable="depth",
         y_variable="energy_loss"
     )
